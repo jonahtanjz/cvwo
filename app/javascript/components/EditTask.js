b610398841f8a19c816b59fe2ea18bcb1f4db152
jonahtanjz <jonahtanjz4884@gmail.com>
2019-12-24 17:13:17 +0800

    Added field to edit tag

--- app/javascript/components/EditTask.js
+++ app/javascript/components/EditTask.js
@@ -7,10 +7,7 @@ function EditTask() {
   const taskId = window.location.pathname.slice(6);
   
   let taskBody = "";
-
-  fetch('/api/todo/' + taskId)
-      .then(response => response.json())
-      .then(data => taskBody = data.data.attributes.body);
+  let taskTag = "";
 
   const handleSubmit = attributes => {
     const requestTasks = async () => {
@@ -18,8 +15,14 @@ function EditTask() {
       // as a header in the request to create a new post.
       // This is needed because with this token, Rails is going to
       // recognize the request as a valid request
-      console.log(attributes);
       const csrfToken = document.querySelector("meta[name=csrf-token]").content;
+      
+      if (attributes.body == "") {
+        attributes.body = taskBody;
+      } else if(attributes.tag == "") {
+        attributes.tag = taskTag;
+      }
+
       const response = await fetch("/api/todo/" + taskId, {
         method: "PATCH",
         credentials: "include",
@@ -38,25 +41,35 @@ function EditTask() {
     requestTasks();
   };
 
-  return (
-    <div>
-      <h2>Edit task</h2>
-      <Formik
-        initialValues={{
-            body: "",
-        }}
-        onSubmit={handleSubmit}
-        render={() => (
-          <Form>
-            <Field type="text" name="body" placeholder={taskBody} />
+  fetch('/api/todo/' + taskId)
+      .then(response => response.json())
+      .then(data => {
+        taskBody = data.data.attributes.body;
+        taskTag = data.data.attributes.tag;      
+      });
 
-            <button type="submit">Change</button>
-            <button type="button" onClick={() => window.history.back()}>Go back</button>
-          </Form>
-        )}
-      />
-    </div>
-  );
+    return ( 
+      <div>
+        <h2>Edit task</h2>
+        <Formik
+          initialValues={{
+              body: "",
+              tag: ""
+          }}
+          onSubmit={handleSubmit}
+          render={() => (
+            <Form>
+              <Field type="text" name="body" placeholder={taskBody} />
+              <Field type="text" name="tag" placeholder={taskTag} />
+
+              <button type="submit">Change</button>
+              <button type="button" onClick={() => window.history.back()}>Go back</button>
+            </Form>
+          )}
+        />
+      </div>
+    );
+
 }
 
 export default EditTask;
